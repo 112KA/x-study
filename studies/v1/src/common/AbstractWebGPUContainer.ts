327807ac16cf4e6f92e4e1d39462aadd8ddc52da
Masayuki Iizuka <mas.iizuka@gmail.com>
2025-08-06 23:21:28 +0900

    refactor: remove AbstractContainer and related classes to streamline architecture

--- studies/v1/src/common/AbstractWebGPUContainer.ts
+++
@@ -1,8 +0,0 @@
-import { WebGPURenderer } from "three/webgpu";
-import { AbstractContainer } from "./AbstractContainer";
-
-export class AbstractWebGPUContainer extends AbstractContainer<WebGPURenderer> {
-	constructor(wrapper: HTMLDivElement) {
-		super(wrapper, WebGPURenderer);
-	}
-}
